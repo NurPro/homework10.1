00db893400204598015a945341382ed6d4a293de
NurPro <uchdobo04@gmail.com>
2023-04-29 12:03:20 +0600

    hw10.1

---
+++ src/utils/Constants.jsx
@@ -0,0 +1,26 @@
+export const product = [
+	{
+		id: '1',
+		productName: 'kiwi',
+		url: 'https://www.frutas-hortalizas.com/img/fruites_verdures/presentacio/14.jpg',
+		price: 120,
+		quantity: 0,
+		copyPrice: 120,
+	},
+	{
+		id: '2',
+		productName: 'apple',
+		url: 'https://5.imimg.com/data5/WA/NV/LI/SELLER-52971039/apple-indian-500x500.jpg',
+		price: 300,
+		quantity: 0,
+		copyPrice: 300,
+	},
+	{
+		id: '3',
+		productName: 'banana',
+		url: 'https://th-thumbnailer.cdn-si-edu.com/6RD8JDrASGTSjdbsJkg-37OY9mQ=/1072x720/filters:no_upscale()/https://tf-cmsv2-smithsonianmag-media.s3.amazonaws.com/filer/d5/24/d5243019-e0fc-4b3c-8cdb-48e22f38bff2/istock-183380744.jpg',
+		price: 250,
+		quantity: 0,
+		copyPrice: 250,
+	},
+];
